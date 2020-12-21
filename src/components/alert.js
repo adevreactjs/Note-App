c5e01fb3b1d0ee29953eb323d32ca72df7773fd8
adevreactjs <adevreactjs@gmail.com>
2020-12-21 22:24:01 +0200

    add context

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,20 +1,24 @@
 import React, {useCallback, useContext} from 'react';
-import { AlertContext } from '../context/alert/alertContext';
+import AlertContext  from '../context/alert/alertContext';
 
 const Alert = () => {
 
     const {alert, hide} = useContext(AlertContext)
 
-    if(!alert) {
+    if(!alert.visible) {
         return null
     }
 
     return (
-    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-        <strong>Внимание!</strong> 
-        {alert.text}
-        <button onClick={hide} type="button" className="btn-close"  aria-label="Close"></button>
-    </div>
+        <div className={`alert alert-${alert.type || "warning"} alert-dismissible"`}>
+  <strong>Внимание!</strong> {alert.text}
+  <button onClick={hide} type="button" className="close" data-bs-dismiss="alert" aria-label="Close">
+      <span aria-hidden='true'>&times;</span>
+  </button>
+</div>
+
+
+
     )
 }
 
